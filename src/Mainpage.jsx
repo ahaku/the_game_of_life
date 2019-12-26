535d7c9704376ed5c9752933fdec3e40a8eacf41
okyandex <avznn@yandex.ru>
2019-12-26 16:38:48 +0300

    grid render

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -1,12 +1,39 @@
 import React, {useState}from 'react';
 import './Mainpage.css';
 
-function Mainpage() {
 
+
+
+function Grid(props) {
+    const {colors} = props;
+    return (
+        <div className="Grid">
+            <div className="GridWrapper" style={{gridTemplateColumns: `repeat(${props.numberCols}, ${props.cellSize})`}}>
+                {props.grid.map((row, i) =>
+                    row.map((col, j) => (
+                        <div 
+                        className="Cell"
+                        style={{
+                            width: props.cellSize,
+                            height: props.cellSize,
+                            backgroundColor: props.grid[i][j] ? colors.filledCellBg : colors.emptyCellBg,
+                            border: '1px solid white'
+                        }}
+                        ></div>
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
+
+function Mainpage() {
+    
     const [numberRows, setNumberRows] = useState(30);
     const [numberCols, setNumberCols] = useState(30);
-    const [cellSize, setSellSize] = useState('20px');
     const [grid, setGrid] = useState(emptyGridGenerate());
+    const [cellSize, setSellSize] = useState('20px');
+    
     const [running, setRunnig] = useState(false);
     const [colors, setColors] = useState({
         'pageBackground': 'white',
@@ -14,17 +41,23 @@ function Mainpage() {
         'filledCellBg': 'green'
     })
 
-    const emptyGridGenerate = () => {
+    function emptyGridGenerate() {
         const rows = [];
         for (let i = 0; i < numberRows; i++) {
             rows.push(Array.from(Array(numberCols), () => 0));
         }
-        setGrid(rows);
+        return rows;
     }
 
     return (
         <div className='Mainpage'>
-MP
+            <Grid
+            numberRows={numberRows}
+            numberCols={numberCols}
+            cellSize={cellSize}
+            grid={grid}
+            colors={colors}
+            ></Grid>
         </div>
     )
 }
