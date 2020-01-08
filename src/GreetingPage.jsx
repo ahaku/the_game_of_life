fbded6689d9306b678b08ec81085bb7c691946da
okyandex <avznn@yandex.ru>
2020-01-08 14:37:46 +0300

    greeting page time of day function added

--- src/GreetingPage.jsx
+++ src/GreetingPage.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import Link from '@material-ui/core/Link';
+import Reference from './components/ButtonRow/Reference/Reference'
+
+const daytimeStyles = {
+    'Day': {
+        backgroundColor: 'lightgreen',
+        color: 'white'
+    },
+    'Evening': {
+        backgroundColor: 'lightblue',
+        color: 'black'
+    },
+    'Morning': {
+        backgroundColor: 'lightgray',
+        color: 'black'
+    },
+    'Night': {
+        backgroundColor: '#212121',
+        color: 'white'
+    }
+}
 
 function GrretingPage() {
+    const [style, setStyle] = React.useState(daytimeStyles[timeOfTheDay()])
 
+    function timeOfTheDay() {
+        const hours = new Date().getHours();
+        if (hours >= 0 && hours < 5) {
+            return 'Night';
+        } else if (hours >= 5 && hours < 12) {
+            return 'Morning';
+        } else if (hours >=12 && hours < 17) {
+            return 'Day';
+        } else {
+            return 'Evening';
+        }
+    }
 
     return (
-        <div className='GreetingPageWrapper'>
-            <h1>Greeting page</h1>
+        <div className='GreetingPageWrapper' style={style}>
             <Link href="/mainpage">Играть</Link>
+            <h1>Greeting page</h1>
         </div>
         
     )
