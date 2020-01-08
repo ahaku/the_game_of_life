ed9814bb1eb0e9c604b484680d9bb617bc10e324
okyandex <avznn@yandex.ru>
2020-01-08 12:42:38 +0300

    add router and greeting page

---
+++ src/GreetingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from '@material-ui/core/Link';
+
+function GrretingPage() {
+
+
+    return (
+        <div className='GreetingPageWrapper'>
+            <h1>Greeting page</h1>
+            <Link href="/mainpage">Играть</Link>
+        </div>
+        
+    )
+}
+
+export default GrretingPage
