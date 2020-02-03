05d6b5d5328cc588bcd40f65bb49f7c1de5980a6
okyandex <avznn@yandex.ru>
2020-02-03 20:23:44 +0300

    grid adaptive size added

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import produce from "immer";
 // import '../../Mainpage.css';
 import "./Grid.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  GridAdaptive: {
+    [theme.breakpoints.down("md")]: {
+      transform: "scale(0.72) "
+    },
+    [theme.breakpoints.down("sm")]: {
+      transform: "scale(0.46) "
+    },
+    [theme.breakpoints.down("xs")]: {
+      transform: "scale(0.23) "
+    }
+  }
+}));
 
 function Grid(props) {
   const { colors } = props;
+  const classes = useStyles();
   return (
     <div className="Grid">
       <div
-        className="GridWrapper"
+        className={`GridWrapper ${classes.GridAdaptive}`}
         style={{
           outline: colors.border,
           gridTemplateColumns: `repeat(${props.numberCols}, ${props.cellSize})`
