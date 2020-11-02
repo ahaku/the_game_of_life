3d26eb69014a59efc2be7a7c9d58422e0e7ad7fc
okyandex <avznn@yandex.ru>
2020-11-02 20:08:28 +0300

    Updates

---
+++ precache-manifest.00a8cf2875f68c51f37e160d5d64eab4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f8ec78723883a3cf27da1f9f20668f",
+    "url": "/the_game_of_life/index.html"
+  },
+  {
+    "revision": "c53b75c6134fe09c9c09",
+    "url": "/the_game_of_life/static/css/main.0464f450.chunk.css"
+  },
+  {
+    "revision": "1d0c5d6724141890371b",
+    "url": "/the_game_of_life/static/js/2.004279f6.chunk.js"
+  },
+  {
+    "revision": "fd56044fbab90a74509299bbf81211c5",
+    "url": "/the_game_of_life/static/js/2.004279f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c53b75c6134fe09c9c09",
+    "url": "/the_game_of_life/static/js/main.e101e24d.chunk.js"
+  },
+  {
+    "revision": "60990418dd081f03b81c",
+    "url": "/the_game_of_life/static/js/runtime-main.edd93275.js"
+  },
+  {
+    "revision": "40f1ac65203e3374bb712820ca500582",
+    "url": "/the_game_of_life/static/media/day.40f1ac65.jpg"
+  },
+  {
+    "revision": "ea64d74e16a76f45900d023c7d2ef927",
+    "url": "/the_game_of_life/static/media/evening.ea64d74e.png"
+  },
+  {
+    "revision": "9232ac1f62e3b212d34dec9f96c3e971",
+    "url": "/the_game_of_life/static/media/morning.9232ac1f.jpg"
+  },
+  {
+    "revision": "4f689d024f817e3706b8c7c67b6142e6",
+    "url": "/the_game_of_life/static/media/night.4f689d02.jpg"
+  }
+]);
